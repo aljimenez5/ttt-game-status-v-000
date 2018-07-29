d6f6b1f3a7d38d9f8aecb1f55e523fbd114b6df4
Arvida Jimenez <arvida.jimenez@gmail.com>
2018-07-29 23:20:54 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -1,6 +1,27 @@
+require 'pry'
+
 # Helper Method
 def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
+
+def won?(board)
+  board_row = []
+  WIN_COMBINATIONS.each do |row_win|
+    row_win.each do |row_win_index|
+      until board_row == row_win  
+        if board[row_win_index] == "X"
+          board_row << row_win_index
+        elsif board[row_win_index] == "O"
+          board_row << row_win_index
+        else
+          return false
+        end
+      end
+    end
+  return board_row
+  end
+end
